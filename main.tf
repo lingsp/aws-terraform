77d544b9679be5de4bb51a62e5249ba645d03b08
lingsp <168424461+lingsp@users.noreply.github.com>
2024-04-30 09:55:48 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,10 @@
 module "records" {
 
-records = local.records
-
 resource "aws_route53_record" "RECORDNAME" {
   zone_id = "ZSHPPV9D06FD5"
   name    = "testrecord.w2sports.net"
   type    = "A"
   ttl     = 3600
-  records  = [
-    "${local.records}"
-  ]
+  records  = var.records
  }
 }
